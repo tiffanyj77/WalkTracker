a74a7a8b1130c8b3a4e00b857c977f2691605a41
tofukitty77 <tiffanyjia77@gmail.com>
2025-08-20 14:07:29 -0400

    Initial Commit

---
+++ WalkTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalkTracker
+//
+//  Created by Tiffany Jia on 8/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalkTracker/WalkTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkTrackerApp.swift
+//  WalkTracker
+//
+//  Created by Tiffany Jia on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WalkTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
